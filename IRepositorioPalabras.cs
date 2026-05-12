7765bc6df934bd12a552aa787b263f0dece8a6fd
carlosalfonso070717-blip <carlosalfonso070717@gmail.com>
2026-05-12 12:27:58 -0600

    Agregar archivos de proyecto.

---
+++ IRepositorioPalabras.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ahorcado
+{
+    public interface IRepositorioPalabras
+    {
+        string ObtenerPalabraAleatoria();
+    }
+}
