1d430432ddee1a0b29ca4566f968c88c3ec038e4
carlosalfonso070717-blip <carlosalfonso070717@gmail.com>
2026-05-12 12:45:08 -0600

    refactor: limpiar Program.cs con inyección de dependencias.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿var juego = new Ahorcado.Juego();
-juego.Jugar();
+﻿using System;
+
+var repositorio = new Ahorcado.PalabrasEnMemoria();
+var motor = new Ahorcado.MotorAhorcado(repositorio);
+var ui = new Ahorcado.ConsolaUI(motor);
+
+Console.WriteLine("=== AHORCADO ===");
+
+while (!motor.Ganado() && !motor.Perdido())
+{
+    ui.MostrarTablero();
+    char letra = ui.PedirLetra();
+
+    if (motor.LetraYaUsada(letra))
+    {
+        ui.MostrarMensaje("Ya usaste esa letra.");
+        continue;
+    }
+
+    motor.RegistrarLetra(letra);
+}
+
+ui.MostrarTablero();
+
+if (motor.Ganado())
+    ui.MostrarMensaje($"\n¡Ganaste! La palabra era: {motor.PalabraSecreta}");
+else
+    ui.MostrarMensaje($"\nPerdiste. La palabra era: {motor.PalabraSecreta}");
+
+if (ui.PreguntarOtravez())
+{
+    var nuevoMotor = new Ahorcado.MotorAhorcado(repositorio);
+    var nuevaUI = new Ahorcado.ConsolaUI(nuevoMotor);
+    // Nota: Para que el ciclo se repita completamente aquí, 
+    // usualmente se envolvería todo en un bucle do-while.
+}
+;
